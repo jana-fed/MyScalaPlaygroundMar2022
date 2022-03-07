b1bd0750e3b8e92aa2611d7c63489339f52d9eb5
Jana <jana.pukach@gmail.com>
2022-03-07 20:28:43 +0200

    Day 3 practice task

---
+++ src/main/scala/Day4Exercise.scala
@@ -0,0 +1,13 @@
+import scala.io.StdIn.readLine
+
+object Day4Exercise extends App {
+val Name = readLine("What is Your name?")
+val WorkYears = readLine("How many years are you working in your firm?").toInt
+  val Wage = readLine("And what is Your monthly wage?").toInt
+  if (WorkYears >= 2) {
+  val Bonus = ((WorkYears - 2) * 0.15 * Wage).toInt
+    println(s"Your Christmas bonus is $Bonus")
+  }
+  else { println("You don't get a bonus!")}
+
+}
